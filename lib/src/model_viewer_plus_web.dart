0deeebab76b9b12a4eeebf0fa0fe4d00fe21b370
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2025-02-21 06:00:02 +0500

    - updated template_grok

--- lib/src/model_viewer_plus_web.dart
+++ lib/src/model_viewer_plus_web.dart
@@ -33,8 +33,8 @@ class ModelViewerState extends State<ModelViewer> {
   /// To generate the HTML code for using the model viewer.
   Future<void> generateModelViewerHtml() async {
     final htmlTemplate = await rootBundle
-        .loadString('packages/model_viewer_plus/assets/template_grok.html');
-        // .loadString('packages/model_viewer_plus/assets/template.html');
+        // .loadString('packages/model_viewer_plus/assets/template_grok.html');
+        .loadString('packages/model_viewer_plus/assets/template.html');
 
     // allow to use elements
     final NodeValidator validator =
